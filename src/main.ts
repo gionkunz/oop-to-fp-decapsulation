b02c7bbdf195fa4eb64ae1d9dae4b6079db995e8
gionkunz <gion@syncrea.ch>
2024-06-04 01:35:59 +0200

    Update to PF

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,39 @@
-export class Circle {
-  constructor(
-    public x: number,
-    public y: number,
-    public radius: number) {
+/*
+  Same logic as before but now we're "decapuslating" and separating the
+  data from the behavior.
 
-  }
+  This FP style approach comes with many benefits:
+  - Clear separation from Model and Behavior
+  - Plain data objects to describe entities
+  - Behavior is more flexible for accepting changes of models, different models etc.
+  - Behavior can mostly be represented as pure functions
+  - Immutability and all it's benefits
+  - You can still use ESModules to structure your code (Model, data and behavior in same module)
+*/
 
-  moveTo(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-  }
-
-  draw() {
-    return `Circle(${this.x}, ${this.y}): Diameter: ${this.radius*2}`;
-  }
+export interface Circle {
+  readonly x: number;
+  readonly y: number;
+  readonly radius: number;
 }
 
-const circle = new Circle(10, 10, 5);
-console.log(circle.draw());
-circle.moveTo(20, 20);
-console.log(circle.draw());
+function moveTo(circle: Circle, x: number, y: number): Circle {
+  return {
+    ...circle,
+    x,
+    y
+  };
+}
+
+export function draw(circle: Circle) {
+  return `Circle(${circle.x}, ${circle.y}): Diameter: ${circle.radius*2}`;
+}
+
+let circle: Circle = {
+  x: 10,
+  y: 10,
+  radius: 5
+};
+console.log(draw(circle));
+circle = moveTo(circle, 20, 20);
+console.log(draw(circle));
